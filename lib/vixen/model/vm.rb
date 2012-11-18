cbe1c856319adcfb2d92a707caccc71cd04c2f9b
Jeff Weiss <jeff.weiss@puppetlabs.com>
2012-11-18 15:16:24 -0800

    return self from VM power ops to allow chaining

--- lib/vixen/model/vm.rb
+++ lib/vixen/model/vm.rb
@@ -9,21 +9,26 @@ class Vixen::Model::VM < Vixen::Model::Base
 
   def power_on
     Vixen::Bridge.power_on handle
+    self
   end
 
   def resume
     power_on
+    self
   end
 
   def suspend
     Vixen::Bridge.suspend handle
+    self
   end
 
   def power_off
     Vixen::Bridge.power_off handle
+    self
   end
 
   def reset
     Vixen::Bridge.reset handle
+    self
   end
 end
